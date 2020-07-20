20045a473638ea76cad78c6d866dacd5cf69c287
“devrobsonsp” <devrobsonsp@gmail.com>
2020-07-19 22:38:29 -0300

    Incluindo exercicios

---
+++ ex1.py
@@ -0,0 +1,16 @@
+#Exercicios simples em python utilizando o CMD do Windows
+cars=100
+space_in_a_car = 4.0
+drivers = 30
+passagers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passagers / cars_driven
+
+#objetivo é manipular variaveis
+print("There are", cars, "cars avaliable")
+print("here are only", drivers, "drivers avaliable")
+
+#gente de deus
+
